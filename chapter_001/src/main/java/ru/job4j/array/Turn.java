444e5def813580cf8151c1581229d591003bb874
Tamerlanaz <tamerlanaz@gmail.com>
2018-06-24 14:27:03 +0300

    6.2. Перевернуть массив. [#4441]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] turn(int[] array) { // объявляем массив
+        for (int index = 0; index < array.length/2; index++) { // цикл for по ячейке массива, от 0 ло половины массива (иначе все вернется назад), шаг 1
+            int i = array[index]; // вводим переменную i и передаем ей значение из ячейки массива
+            array[index] = array[array.length - 1 - index]; //передаем нашей ячейке значение "зеркальной" ячейки с конца массива
+            array[array.length - 1 - index] = i;  // передаем "зеркальной" ячейке значение переменной i.
+        }
+        return array;
+    }
+}
